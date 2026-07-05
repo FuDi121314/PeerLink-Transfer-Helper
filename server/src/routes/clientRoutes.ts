5603c175e652fc7363ca1024caef9b38ebf37641
FuDi <fufongchung@gmail.com>
2026-07-05 21:03:48 +0800

    Big change from root

--- server/src/routes/clientRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/', (_req, res) => {
-  res.json([{ username: 'anonymous', connectedAt: new Date().toISOString() }]);
-});
-
-export { router as clientRoutes };
--- server/src/utils/fileUtils.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export function ensureDirectory(dir: string) {
-  const targetDir = path.join(__dirname, '../../../', dir);
-  if (!fs.existsSync(targetDir)) {
-    fs.mkdirSync(targetDir, { recursive: true });
-  }
-}
